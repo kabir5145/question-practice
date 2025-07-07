92d003219e46ca8fc760b0ef883335a6731a4322
kabir5145 <kabir10112006@gmail.com>
2025-07-07 17:18:45 +0530

    for practicing java questions

---
+++ com/company/java_exercise_1.java
@@ -0,0 +1,98 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class java_exercise_1 {
+    //question 1
+//    public static int num(int a ,int b ,int c ){
+//        int avg = (a+b+c)/3;
+//        return avg;
+//    }
+
+    //question 2
+    /*
+    public static int odd(int n ){
+        int sum=0;
+        n = 9;
+        for(int i = 1;i<=n;i++){
+            if(i%2 !=0){
+                sum +=i;
+            }
+        }
+        return sum;
+    }
+     */
+
+    //question 3
+//    public static int numbers(int a ,int b ) {
+//       if(a<b){
+//           return b;
+//       }else {
+//           return a;
+//       }
+//    }
+
+    //question 4
+//    public static double circumference(int r){
+//        return Math.PI*2*r;
+//    }
+
+    //question 5
+    /*
+    public static int age(int n){
+        if(n<18){
+            System.out.println("You are not eligible for giving the vote");
+        }else {
+            System.out.println("You can give the vote");
+        }
+        return n;
+    }
+     */
+
+    //question 7
+
+
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //question 1
+//        int a = sc.nextInt();
+//        int b = sc.nextInt();
+//        int c = sc.nextInt();
+//        int avg = num(a,b,c);
+//        System.out.println("average of three numbers is: "+avg);
+
+        //question 2
+//        int limit = 10;
+//        int result = odd(limit);
+//        System.out.println("sum  of "+limit+" is "+result);
+
+        //question 3
+//        int a = sc.nextInt();
+//        int b = sc.nextInt();
+//        int result = numbers(a,b);
+//        System.out.println("the greatest num is: "+result);
+
+
+        //question 4
+//        int r = sc.nextInt();
+//        double result= circumference(r);
+//        System.out.println("the circumference of the circle is : "+result);
+
+        //question 5
+        /*
+        System.out.println("Enter your age :-");
+        int n = sc.nextInt();
+        int result = age(n);
+        System.out.println("Thank you !");
+         */
+
+        //question 6
+//        int i = 0;
+//       do{
+//           System.out.println(i);
+//           i++;
+//       }while (i>0);
+    }
+}
