7bad4a76d5cdec4dee9165d6310814cae65edc8d
kabir5145 <kabir10112006@gmail.com>
2025-07-15 15:10:20 +0530

    revision ongoing

--- arrays.java
+++ arrays.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class arrays {
     public static void main(String[] args) {
 //    int [] marks = {343,56,738,8679};
@@ -12,8 +14,33 @@ public class arrays {
 //        double average = sum / numbers.length;
 //        System.out.println("average of the numbers is:"+ average);
 
-        //question
+        //question 1
+//        int [] number = {2,3,3,1};
+//        int sum = 0;
+//        for(int  i: number){
+//            sum += i;
+//            System.out.println(sum);
+//        }
 
+        //question 2
+//        int[][] pattern = new int[5][5];
+//        //input
+//        for (int i = 0; i < 10; i++) {
+//            //output
+//            for (int k = 0; k < 10; k++) {
+//                System.out.print("-"+ " ");
+//            }
+//            System.out.println();
+//        }
+
+        //question 4
+        int [] avg ={12,-34,98} ;
+        int sum = 0;
+        for(int i = 0;i<avg.length;i++){
+            sum = sum +avg[i];
+        }
+        double result = sum/ avg.length;
+        System.out.println(result+" This is the average of three numbers");
         }
     }
 
