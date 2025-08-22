fb7d50b016f99b844cd1f3760a8105eff6f1e5ac
kabir5145 <kabir10112006@gmail.com>
2025-08-22 23:27:51 +0530

    1 logical question

--- com/company/logical_question.java
+++ com/company/logical_question.java
@@ -4,103 +4,16 @@ import java.util.*;
 
 public class logical_question {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        int a = 0;
+        int b = 1;
+        int d = 10;
 
-/*
-        HashMap<String, List<String>> replacements = new HashMap<>();
-        replacements.put("sad", List.of("happy", "khush", "bahut khush"));
-        replacements.put("angry", List.of("calm", "gusse mein"));
-        replacements.put("hate", List.of("love", "prem"));
-        replacements.put("tired", List.of("energetic", "joshila"));
-
-        System.out.println("Enter a sentence:");
-        String input = sc.nextLine();
-
-        String[] words = input.split(" ");
-
-        Random r = new Random();
-        for (int i = 0; i < words.length; i++) {
-
-            if (replacements.containsKey(words[i])) {
-                List<String> replacementWords = replacements.get(words[i]);
-                int index = r.nextInt(replacementWords.size());
-                words[i] = replacementWords.get(index);
-            }
-        }
-
-        // Join the words back into a sentence
-        String output = String.join(" ", words);
-
-        // Print the modified sentence
-        System.out.println("Modified sentence:");
-        System.out.println(output);
- */
-//        Random r = new Random();
-//        System.out.println("Enter a sentence: ");
-//        String input = sc.nextLine();
-//
-//        //String[] inputWords ={"hello","yes","no","ok","nice"};
-//        String[] randomWords ={"good ","bad","honest","lier","dangerous"};
-//
-//        String[] words = input.split(" ");
-//
-//
-//        for (int i = 0; i < words.length; i++) {
-//            int randomIndex = r.nextInt(randomWords.length);
-//            words[i] = randomWords[randomIndex];
-//        }
-//
-//        // Step 5: Join the new words into a sentence
-//        String newSentence = String.join(" ", words);
-//        System.out.println("Randomized sentence: " + newSentence);
-
-        //question2
-/*
-        String input = "00101011010101";
-        int zeroCount = 0;
-
-        for(int i = 0;i<input.length();i++){
-            if(input.charAt(i)=='0' ){
-                zeroCount++;
-            }
-        }
-        for(int i= 0;i<zeroCount;i++ ){
-            System.out.print("0");
-        }
-        for(int i =0;i<input.length()-zeroCount;i++){
-            System.out.print("1");
-        }
- */
-
-        //question 4
-        /*
-        int b = 5;
-        int n = 0;
-        for (int i = 0;i<=b;i++){
-            for (int k = 0;k<=n;k++){
-                System.out.print(k);
-            }
-            System.out.println();
-            n++;
-        }
-         */
-
-        int[] number = {12, 23, 1, 23, 11};
-        System.out.println("Insert any number -->");
-        int s = sc.nextInt();
-        boolean fund = false;
-        for (int i = 0; i < number.length; i++) {
-            if (number[i] == s) {
-                fund = true;
-                break;
-            }
-        }
-            if (fund) {
-                System.out.println("yes");
-            } else {
-                System.out.println("no");
-            }
+        System.out.print(a + " " + b + " ");
+        for(int i = 2;i<d;i++){
+            int sum = a+b;
+            System.out.print(sum+" ");
+            a = b;
+            b = sum;
         }
     }
-
-
+}
