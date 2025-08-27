2736680f907ecaed063ca8269adb38f4fd03235f
kabir5145 <kabir10112006@gmail.com>
2025-08-28 00:35:07 +0530

    Array question

--- com/company/function_question.java
+++ com/company/function_question.java
@@ -1,39 +1,27 @@
 package com.company;
 
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class function_question {
-public static void calculator(double n , double m){
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Enter the first number:");
-    m = sc.nextDouble();
-    System.out.println("Enter the second number:");
-    n = sc.nextDouble();
-    System.out.println("Enter the operator which you want to use are +,-,*,/:");
-    char operator  = sc.next().charAt(0);
-    double result = calculate(m, n, operator);
-    System.out.println(result);
-    }
-
-    public static double calculate(double m, double n, char operator) {
-        return switch (operator) {
-            case '+' -> m + n;
-            case '-' -> m - n;
-            case '*' -> m * n;
-            case '/' -> {
-
-                if(n==0.0) {
-                    throw new RuntimeException("error");
-                }
-                yield m/n;
-            }
-            default -> throw new RuntimeException("Invalid operator");
-        };
-    }
-
     public static void main(String[] args) {
-    calculator(2,3);
+        int []arr ={12,43,565,2,21,43};
+        System.out.println("The given array is: "+ Arrays.toString(arr));
 
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for(int num : arr){
+            if(num>largest){
+                secondLargest = largest;
+                largest = num;
+            } else if (num > secondLargest && num != largest) {
+                secondLargest = num;
+            } else if (num==secondLargest) {
+                System.out.println("There are two common second largest words in an array");
+            }
+        }
+        System.out.println("The second largest number is : "+secondLargest);
     }
 }
